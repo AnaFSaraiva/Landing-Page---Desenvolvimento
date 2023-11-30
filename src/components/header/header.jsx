fb101660e96d077054acfe788415ba539bc6a6f5
AnaFSaraiva <saraivanapaula2@gmail.com>
2023-11-30 01:48:41 -0300

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.scss'
+
+function Header() {
+  return (
+    <div id='head'>
+      < img href="" alt="" />
+      <nav>
+        <a href='https://www.op.gg/champions/morgana/build?tier=silver'>Conhecer</a>
+        <a href='https://www.op.gg/champions/morgana/build?tier=silver'>Projetos</a>
+        <a href='https://www.op.gg/champions/morgana/build?tier=silver'>Contato</a>
+      </nav>
+
+    </div>
+  )
+}
+
+export { Header }
---
+++ src/components/main/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './main.scss'
+
+function Main() {
+
+  return (
+    <>
+      <div className='first-section'>
+        <h1>Sobre</h1>
+        <p>Ex-enfermeira que se apaixonou pelo mundo da tecnologia e desenvolvimento de sistemas.</p>
+        <img alt='gif do bob esponja' src='https://i.giphy.com/media/SKGo6OYe24EBG/giphy.webp' />
+      </div>
+    </>
+  )
+
+}
+
+export { Main }
