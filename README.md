538e6e09beb97d58d4a930828694ec0258a1ad36
AnaFSaraiva <saraivanapaula2@gmail.com>
2023-12-01 13:42:42 -0300

    feat new section

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ⚠ EM DESENVOLVIMENTO ⚠ Landing Page
 
 Criação de uma Landing Page em React.
+
+projeto feito em mobile first
+
+> **DEPLOY** -> npm start
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,13 +4,28 @@ import './main.scss'
 function Main() {
 
   return (
-    <>
-      <div className='first-section'>
-        <h1>Sobre</h1>
-        <p>Ex-enfermeira que se apaixonou pelo mundo da tecnologia e desenvolvimento de sistemas.</p>
+    <div className='main-section'>
+      <section className='first-section'>
+        <div className='text-block'>
+          <h1>Sobre</h1>
+          <p>Ex-enfermeira que se apaixonou pelo mundo da tecnologia e desenvolvimento de sistemas.</p>
+        </div>
         <img alt='gif do bob esponja' src='https://i.giphy.com/media/SKGo6OYe24EBG/giphy.webp' />
-      </div>
-    </>
+      </section>
+
+      <section className='second-section'>
+        <div className='text-block'>
+          <h1>Projetos</h1>
+          <div className=''>
+            <p>Cozinhe o macarrão instantâneo normalmente.Em uma panela, coloque a manteiga, o tomate, a cebola e o tempero em pó do macarrão instantâneo para fritar, sempre mexendo bem para não empelotar.Depois despeje o creme de leite e misture o molho ao macarrão instantâneo.</p>
+          </div>
+        </div>
+        <img alt='gif do bob esponja' src='https://i.giphy.com/media/WoWm8YzFQJg5i/200.webp' />
+
+
+      </section>
+
+    </div>
   )
 
 }
